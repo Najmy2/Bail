ed8de4b01ed34b3ceeb43b4b451122731ca51a20
Najmy2 <94039459+Najmy2@users.noreply.github.com>
2025-02-02 13:35:49 +0700

    Update messages-send.ts

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -533,10 +533,10 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 				const stanza: BinaryNode = {
 					tag: 'message',
 					attrs: {
-                    id: msgId,
-                    type: isNewsletter ? getTypeMessage(message) : 'text',
-                    ...(additionalAttributes || {})
-                },
+						id: msgId,	
+						type: isNewsletter ? getTypeMessage(message) : 'text',
+						...(additionalAttributes || {})
+					},
 					content: binaryNodeContent
 				}
 				// if the participant to send to is explicitly specified (generally retry recp)
